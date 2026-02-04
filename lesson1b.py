5ed550a1ce77ee6a7abe210784ffeffe1ddbb643
tash <plutoneryme@gmail.com>
2026-02-04 13:22:44 +0300

    added lesson1b

---
+++ lesson1b.py
@@ -0,0 +1,18 @@
+#python data types
+#it is a series of characters that are inside of quotes.
+name = "Joseph Kainemgunga"
+print(name)
+print(type(name))
+
+#Numeric
+# Int - indicates a whole number in python. For an integer, we don't use quotes.
+age = 39
+print("The age of a person is:" ,age)
+print(type(age))
+
+#float - this is a number with a decimal.
+coordinates = 15.4524
+print("The coordinates are: ", coordinates)
+print(type(coordinates))
+
+#search and come up with an example of a complex data type
