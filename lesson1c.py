edeff558e460f58e505846d526203fba2a4d995e
tash <plutoneryme@gmail.com>
2026-02-04 14:15:48 +0300

    added files

---
+++ lesson1c.py
@@ -0,0 +1,46 @@
+#Arithmetic operation
+#Addition
+number1 = 10
+number2 = 20
+addition = number1 + number2
+print("The sum of the two numbers is: ", addition)
+
+#subtraction
+number3 = 50
+number4 = 15
+difference = number3 - number4
+print("The difference of the number is :",difference)
+
+
+#multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("The product of the number is : ",product)
+
+#division
+number7 = 21
+number8= 3
+division = number7 / number8
+
+#modulus- This is the remainder oof a number when subdivided by another one.
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus number is : ",modulus)
+
+#Floor division - This is the number of times a number subdivides another ignoring the remainder.
+floor = number9 // number10
+print("The floor divisionof the two numbers is: ", floor)
+
+#exponent(**)
+#The power of of a number 
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is: ", exponent)
+
+
+
+
+
+
---
+++ lesson1d.py
@@ -0,0 +1,14 @@
+#create a python program that is able to calculate the simple interest given the principle as 50000, rate as 55 and time as 8 years. SI = (p*r*t)/100
+
+p = 50000
+r = 5
+t=8
+SI=(p*r*t)/100
+print("The simple interest is: ", SI)
+
+
+
+#Assignment
+#1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters = (weight)/(height squared)
+# 2. Find the Area and perimeter of a rectangle whose length is 48cm and width is 25cm
+# Research on python list, tuple and Dictionary Data types.
