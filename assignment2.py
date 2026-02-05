dad44027de2fc17c8a53bc6f8923f1252309e51f
tash <plutoneryme@gmail.com>
2026-02-05 18:27:55 +0300

    added files

---
+++ assignment2.py
@@ -0,0 +1,7 @@
+#A tuple of eight planets.
+
+planets=("Mercury", "Venus","Earth","Mars","Jupiter","Saturn","Uranus","Pluto")
+print(planets)
+
+#print the planet from index 2 to 6
+print(planets[2:7])
