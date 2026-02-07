faf4f148f9db768230e961fc3855cf94aec1b40d
tash <plutoneryme@gmail.com>
2026-02-07 19:15:00 +0300

    added files

---
+++ assignment3.py
@@ -0,0 +1,26 @@
+gross_income = float(input("Enter gross_income: "))
+
+if gross_income> 0 and gross_income <= 5999:
+ print("The monthly contribution is: ,",150.00)
+elif gross_income >=6000 and gross_income <= 7999:
+ print("The monthly contribution is: ",300.00)
+elif gross_income >=8000 and gross_income <= 11999:
+ print("The monthly contribution is: ",400.00)
+elif gross_income >=12000 and gross_income <= 14999:
+ print("The monthly contribution is: ",500.00)
+elif gross_income >=15000 and gross_income <= 19999:
+ print("The monthly contribution is: ",600.00)
+elif gross_income >=20000 and gross_income <= 24999:
+ print("The monthly contribution is: ",750.00)
+elif gross_income >=25000 and gross_income <= 29999:
+ print("The monthly contribution is: ",850.00)
+elif gross_income >=30000 and gross_income <= 49999:
+ print("The monthly contribution is: ",1000.00)
+elif gross_income >=50000 and gross_income <= 99999:
+ print("The monthly contribution is: ",1500.00)
+else:
+ print("The monthly contribution is: ", 2000) 
+     
+   
+   
+  
